1e807122a9b0e254d1367396a8af9a5cd29c17a3
Anup Narvekar <56749510+AnupNarvekar@users.noreply.github.com>
2021-09-25 11:50:48 +0530

    Update iterator.cpp

--- Basics/STL Containers/iterator.cpp
+++ Basics/STL Containers/iterator.cpp
@@ -8,9 +8,9 @@ int main()
 {
     vector <int> v1 = {1,2,3,4,5}; // defining a vector
 
-    vector <int> :: iterator itr; // defining a iterator for vector v1
+    vector <int> :: iterator itr; // defining a iterator for vector
 
-    for(itr=v1.begin(); itr < v1.end(); itr++)
+    for(auto itr=v1.begin(); itr < v1.end(); itr++)
     {
         cout << *itr << "\t";
     }
