c7f89162200616e07a2bcf116e7839d0c1768617
AnupNarvekar <narvekaranup321@gmail.com>
2023-04-27 03:12:39 +0530

    Renamed functions

--- Basics/Pointers.cpp
+++ Basics/Pointers.cpp
@@ -7,12 +7,12 @@ int addByValue(int x, int y){
 };
 
 // Pass argument by reference using pointer variables
-int addByReference_pointer (int* x, int* y){
+int addUsingPointers (int* x, int* y){
     return *x + *y;
 };
 
 // Pass arguments by reference using referance variables
-int addByReference_reference(int& x, int& y){
+int addUsingReference(int& x, int& y){
     return x + y;
 };
 
@@ -25,7 +25,7 @@ int main(){
     int* bPtr = &b; // bPtr <-- address of b
 
     cout << "a + b by val: " << addByValue(a, b) << endl; // 15
-    cout << "a + b by ref -> pointer: " << addByReference_pointer(aPtr, bPtr) << endl; // 15
-    cout << "a + b by ref -> reference: " << addByReference_reference(a, b) << endl; // 15
+    cout << "a + b by ref -> pointer: " << addUsingPointers(aPtr, bPtr) << endl; // 15
+    cout << "a + b by ref -> reference: " << addUsingReference(a, b) << endl; // 15
     
 }
