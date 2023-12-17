b5ac2c9f1b58ab4444c0550c2270cb4510e3618e
AnupNarvekar <narvekaranup321@gmail.com>
2023-12-18 05:01:06 +0530

    Added array pointer example

--- Basics/Pointers.cpp
+++ Basics/Pointers.cpp
@@ -19,10 +19,15 @@ int addUsingReference(int& x, int& y){
 int main(){
     int a = 5;
     int b = 10;
+    int arr[] = {1,2,3,4,5};
 
     // Declare and initialize pointer variables
     int* aPtr = &a; // aPtr <-- address of a
     int* bPtr = &b; // bPtr <-- address of b
+    int* arrPtr = arr; // arrays return address of first index
+
+    cout << arr << endl;
+    cout << arrPtr << endl;
 
     cout << "a + b by val: " << addByValue(a, b) << endl; // 15
     cout << "a + b by ref -> reference: " << addUsingReference(a, b) << endl; // 15
@@ -30,5 +35,5 @@ int main(){
     cout << "a + b by ref -> pointer: " << addUsingPointers(aPtr, bPtr) << endl; // 15
     // OR
     cout << "a + b by ref -> pointer: " << addUsingPointers(&a, &b) << endl; // 15 | Can also pass the address directly without creating a pointer variable
-    
+
 }
